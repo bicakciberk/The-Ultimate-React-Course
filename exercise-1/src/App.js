c35c48c44b24378553eee8977d0eb96177ae1b48
Berk Bıçakcı <120296952+bicakciberk@users.noreply.github.com>
2024-01-24 20:40:40 +0300

    added exercise-1. i learned components

---
+++ exercise-1/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Flashcards from "./components/Flashcards";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Flashcards />
+    </div>
+  );
+}
